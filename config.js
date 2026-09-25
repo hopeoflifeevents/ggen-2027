8a7421cdca0bf3b433d59481ff02743f2bbdade9
hopeoflifeevents <hopeoflifeevents@gmail.com>
2026-09-25 09:09:54 +0800

    Create config.js

---
+++ config.js
@@ -0,0 +1,7 @@
+// =============================================================
+// GGEN 2027 — CENTRAL ENVIRONMENT CONFIGURATION
+// =============================================================
+const CONFIG = {
+  // Update this URL ONLY in this file when redeploying
+  APPS_SCRIPT_API_URL: "https://script.google.com/macros/s/AKfycbxOB6MC6oCHNcOEMwf2KC8eTfhFED6WvRr5jnjL4yi50nRvmADrTi2TWapH3Q8ozg88Fw/exec"
+};
